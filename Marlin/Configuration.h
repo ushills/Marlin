651ab13247670ae1cfa50b5a883ad762cb662e6d
ushills <web@ushills.co.uk>
2019-05-16 09:49:33 +0000

    Finalised travel limits

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -908,7 +908,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -12
-#define Y_MIN_POS 8
+#define Y_MIN_POS 3
 #define Z_MIN_POS 0
 #define X_MAX_POS (-X_MIN_POS+X_BED_SIZE)
 #define Y_MAX_POS (-Y_MIN_POS+Y_BED_SIZE)
